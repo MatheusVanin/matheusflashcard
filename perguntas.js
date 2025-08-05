6177eb8a348c04a494a660f082607b6eb679bc7e
MatheusVanin <vanin.matheus@escola.pr.gov.br>
2025-08-05 10:51:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital do PR?',
+    'Curitiba'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do RS?',
+    'Porto alegre'
+)
+
+criaCartao(
+    'Matematica',
+    'Quanto é 5x5?',
+    '25'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz boa tarde em Inglês?',
+    'Good afternoon'
+)
